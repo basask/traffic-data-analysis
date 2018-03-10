c360923dfb2bd5eb43c04478193d9b17477d5bb1
Basask <basask@gmail.com>
2018-03-10 16:17:38 -0300

    Adding detection simulation notebook

--- wrappers/mock_lib.py
+++ wrappers/mock_lib.py
@@ -1,29 +1,33 @@
 IDDLE_DATA = 0
-SENSOR_TRESHOLD = 2
+FALSE_NEGATIVE_TOLERANCE = 0
+FALSE_POSITIVE_TOLERANCE = 2
 
 seq_count = 0
-last_seq_count = 0
+fn_count = 0
 
 def process_sensor_data(data):
     global seq_count
-    global last_seq_count
+    global fn_count
 
     if data == IDDLE_DATA:
-        if seq_count > 0 and seq_count > SENSOR_TRESHOLD:
-            last_seq_count = seq_count
-        seq_count = 0
-        return
+        if fn_count >= FALSE_NEGATIVE_TOLERANCE:
+            fn_count = 0
+            ret = seq_count if seq_count > FALSE_POSITIVE_TOLERANCE else 0
+            seq_count = 0
+            return ret
     seq_count += 1
+    return 0
 
 
 def replay_sensor_readings(param):
     global seq_count
-    global last_seq_count
+    global fn_count
 
     seq_count = 0
-    last_seq_count = 0
+    fn_count = 0
 
+    ret = 0
     for i in param:
-        process_sensor_data(i)
-
-    return last_seq_count
+        res = process_sensor_data(i)
+        ret = res if res > 0 else ret
+    return ret
